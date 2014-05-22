478e5c690315754ebdc3b462a8b8ed7139c1b24d
poemusica <heatheramahan@gmail.com>
2014-05-22 14:04:54 -0700

    Adds room drawing feature.

--- tatami.py
+++ tatami.py
@@ -1,32 +1,61 @@
 import turtle
+import random
+
+#===========================================================
 
 #GLOBAL VARIABLES
+
+#fav colors
+BGCOLOR = '#aa995a'
+GREEN = '#96a20e'
 
 #set up screen and pen (turtle).
 #turtle.colormode(255)
 WN = turtle.Screen()
 WN.title ('Tatami')
-WN.bgcolor('#aa995a')
+WN.bgcolor(BGCOLOR)
 
-def draw_rectangle(t, w, l):
-    t.fillcolor('#96a20e')
-    t.begin_fill()
-    t.forward(l)
-    t.left(90)
-    t.forward(w)
-    t.left(90)
-    t.forward(l)
-    t.left(90)
-    t.forward(w)
+#===========================================================
+
+#generate a random hex color.
+def gen_hex_color():
+    color = ['#']
+    for i in range(6):
+        color.append(random.choice('0123456789ABCDEF'))
+    return ''.join(color)
+
+#draw rectangle.
+def draw_rectangle(t, w, l, fill=None):
+    t.down()
+    if fill:
+        t.begin_fill()
+    for x in range(2):
+        t.forward(w)
+        t.left(90)
+        t.forward(l)
+        t.left(90)
     t.end_fill()
-    t.left(90)
 
 
-def tatami_gen():
-    t = turtle.Turtle()
-    t.width(7)
-    draw_rectangle(t, 200, 100)
+def draw_room(rm_w, rm_l):
+    room = turtle.Turtle()
+    room.pen(pencolor="black", pensize=7, shown=False, pendown=False, speed=4)
+    #center room
+    room.setpos( (-rm_w / 2), (-rm_l / 2) )
+    draw_rectangle(room, rm_w, rm_l)
+
+
+def size_of_tiles(w, l):
+    tile_short = w/200
+    tile_long = tile_short * 2
+
+
+def tatami_gen(w, l):
+    draw_room(w, l)
+    size_of_tiles(w, l)
+
     WN.exitonclick()
 
+#===========================================================
 
-tatami_gen()
+tatami_gen(600, 600)
