5e047bc60d4437956bf8ea5be5a4ed44fe818448
poemusica <heatheramahan@gmail.com>
2014-05-21 17:57:36 -0700

    starts with basic rectangle drawing

--- tatami.py
+++ tatami.py
@@ -1,1 +1,32 @@
-test
+import turtle
+
+#GLOBAL VARIABLES
+
+#set up screen and pen (turtle).
+#turtle.colormode(255)
+WN = turtle.Screen()
+WN.title ('Tatami')
+WN.bgcolor('#aa995a')
+
+def draw_rectangle(t, w, l):
+    t.fillcolor('#96a20e')
+    t.begin_fill()
+    t.forward(l)
+    t.left(90)
+    t.forward(w)
+    t.left(90)
+    t.forward(l)
+    t.left(90)
+    t.forward(w)
+    t.end_fill()
+    t.left(90)
+
+
+def tatami_gen():
+    t = turtle.Turtle()
+    t.width(7)
+    draw_rectangle(t, 200, 100)
+    WN.exitonclick()
+
+
+tatami_gen()
