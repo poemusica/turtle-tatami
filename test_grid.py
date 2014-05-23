773779f2f676b79a1c926c6c323a09b2ec3b0292
poemusica <heatheramahan@gmail.com>
2014-05-23 10:58:08 -0700

    fully functional grid of clickable squares with fill-changing spacebar.

--- test_grid.py
+++ test_grid.py
@@ -10,65 +10,87 @@ def gen_hex_color():
         color.append(random.choice('0123456789ABCDEF'))
     return ''.join(color)
 
-#helps the script exit gracefully.
-def run():
-    global RUNNING
-    RUNNING = False
-
 #change fill color. next time fill color is toggled, the new color will be used.
 def new_fill():
-    global CURRENT_FILL, MEMORY, SWITCH
-    SWITCH = False
+    global CURRENT_FILL, MEMORY
     CURRENT_FILL = gen_hex_color()
     MEMORY = CURRENT_FILL
 
-#toggle fill color of square: current fill vs. no fill (bgcolor).
-def toggle_fill(x=None, y=None):
-    global CURRENT_FILL, MEMORY, BGCOLOR, SWITCH
-    SWITCH = True
-    if x != None:
+#creates a callback function within the scope of t. 
+def filler_maker(t):
+    def filler(x, y):
+        global CURRENT_FILL, MEMORY, BGCOLOR
         if CURRENT_FILL == BGCOLOR:
             CURRENT_FILL = MEMORY
         else:
             CURRENT_FILL = BGCOLOR
+        t.fillcolor(CURRENT_FILL)
+    return filler
 
 #draw a square and allow the fill color to be toggled or changed to a new random color.
-def draw_square(n):
+#note: side of square equals stretchfactor times 20. position of shape is calculated from its centerpoint.
+def draw_square(sside, spos):
+    stretch = sside / 20
     t = turtle.Turtle()
-    t.pen(pensize=2, resizemode='user', stretchfactor=(n, n), outline=2, fillcolor=BGCOLOR)
+    t.pen(pensize=2, resizemode='user', stretchfactor=(stretch, stretch), outline=2, fillcolor=BGCOLOR,
+        shown=True, pendown=False, speed=0)
     t.shape('square')
-    #center
-    start_coord = -n / 2
-    t.setpos(start_coord, start_coord)
-    while RUNNING:
-        global WN
-        WN.onkey(new_fill, "space")
-        WN.onkey(run, 'q')
-        WN.listen()
-        t.onclick(toggle_fill)
-        global SWITCH
-        if SWITCH == True:
-            t.fillcolor(CURRENT_FILL)
-    WN.exitonclick()
+    t.setpos(spos['x'], spos['y'])
+    t.onclick(filler_maker(t))
+
+#draw a grid of squares
+def draw_grid(m):
+    square_side = 100
+    grid_side = m * square_side
+    start_coord = -(grid_side/2 - square_side/2)
+    square_pos = {'x': start_coord, 'y':start_coord}
+    max_y = start_coord + (square_side * (m - 1))
+
+    WN.tracer(0)
+    for i in range(m * m):
+        draw_square(square_side, square_pos)
+        if square_pos['y'] < max_y:
+            square_pos['y'] = square_pos['y'] + square_side
+        else:
+            square_pos['y'] = start_coord
+            square_pos['x'] = square_pos['x'] + square_side
+    WN.tracer(1)
+
+def get_user_input():
+    while True:
+        user_input = raw_input('This program creates an n x n grid. Enter a number to set n. \n')
+        try:
+            int(user_input)
+        except ValueError:
+            print "Please enter a whole number."
+        else:
+            return int(user_input)
+
+def main():
+    m = get_user_input()
+    draw_grid(m)
+    print "Click a square to toggle fill. \nTo switch the fill color press the spacebar."
+    WN.onkey(new_fill, "space")
+    WN.listen()
+    turtle.mainloop()
 
 #===========================================================
 
 #GLOBAL VARIABLES
 
 BGCOLOR = '#aa995a'
+
+#state keepers
 MEMORY = gen_hex_color()
 CURRENT_FILL = BGCOLOR
-SWITCH = False
-RUNNING = True
 
 #set up screen
 WN = turtle.Screen()
 WN.title ('Square Grid Playground')
 WN.bgcolor(BGCOLOR)
 
-
 #===========================================================
 
-draw_square(10)
-
+#let's do this!
+main()
 
